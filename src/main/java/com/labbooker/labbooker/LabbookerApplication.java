80f0983e977fb4b99eb7f7ac6d09a68487c2658c
Abdelali Mouida <abdelalimouida7@gmail.com>
2026-09-19 22:52:00 -0400

    Initial commit

---
+++ src/main/java/com/labbooker/labbooker/LabbookerApplication.java
@@ -0,0 +1,13 @@
+package com.labbooker.labbooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabbookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabbookerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labbooker/labbooker/LabbookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labbooker.labbooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabbookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
